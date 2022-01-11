7b11a496b15a4e565d155cedf824f863d83e3278
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-10 22:32:23 -0500

    commit tree e4345355e4b5e648724af14c55a53dcd275b22cb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') { 
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+//to call functions before they appear in the code is called hoisting
+console.log(mondayWork("work from home"));
+console.log(mondayWork());
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(value = "special") {
+        return `You are ${visualFlair}${value}${visualFlair}!`;
+    };
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
+console.log(wrapAdjective()());
